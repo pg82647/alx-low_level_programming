e2ffdaea4d61857d334caf847e4670f0854283a9
Etebom Ntuk <ntuketebom@gmail.com>
2022-10-18 13:12:58 -0700

    is ALPha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,19 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _isalpha - tests whether a character is from the English alphabet.
- * @c: character to be checked.
- * Return: 1 if the character is an English character.
- * 0 if the character is not an English character.
+ * _isalpha - function description
+ * @c: the subject test character
+ *
+ * Return: 1 if lowercase else 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
-
+	}
 }
