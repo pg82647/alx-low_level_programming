5fe221db9e4db5fd9f8b9b22559894b50bd8d9a5
Etebom Ntuk <ntuketebom@gmail.com>
2022-10-20 07:55:17 -0700

    Numbers have Life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_number - print integer
+ * @n: The number to print
+ */
+void print_number(int n)
+{
+	unsigned int x;
+	unsigned int i;
+	unsigned int y;
+
+	i = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		y = -n;
+	}
+	else
+	{
+		y = n;
+	}
+	x = y;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		while (x >= 10)
+		{
+			i = i * 10;
+			x = x / 10;
+		}
+		while (i > 1)
+		{
+			_putchar((y / i) + '0');
+			y = y % i;
+			i = i / 10;
+		}
+		_putchar(y + '0');
+	} 
+}
