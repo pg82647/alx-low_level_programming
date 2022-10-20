0398ac2af02379546dd8aaa79402d9ba59d71f73
Etebom Ntuk <ntuketebom@gmail.com>
2022-10-20 12:19:49 -0700

    The only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,26 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * more_numbers - function that checks for uppercase character
+ * Return: 0
  */
-int main(void)
+void more_numbers(void)
 {
-    more_numbers();
-    return (0);
+
+	int a = 0;
+	int b = 0;
+
+	while (b <= 9)
+	{
+		while (a <= 14)
+		{
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			a++;
+
+		}
+	_putchar('\n');
+	b++;
+	a = 0;
+	}
 }
