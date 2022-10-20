66717d6c4e1a809b627f27569205a663daf17b28
Etebom Ntuk <ntuketebom@gmail.com>
2022-10-20 05:21:23 -0700

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - checks if a character is digit or not
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: the ?factor
+ */
+
+int mul(int a, int b)
+{
+	return ((int)(a * b));
+}
