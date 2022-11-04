8ecd2a6055a8ba1f99d32ff11e4a2d0c4ef0244c
etetuk <etebom.ntuk.pg82647@unn.edu.ng>
2022-11-04 21:53:50 +0100

    What is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function description
+ * @argc: arg count
+ * @argv: args vector
+ * Return: success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *argv);
+	return (0);
+}
